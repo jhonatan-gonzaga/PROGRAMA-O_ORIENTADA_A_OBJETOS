1714e2be7645fc59d0d5a5c8fef79354c26565e7
Jhonatan Gonzaga <jhonatangonzagagaldino@gmail.com>
2026-05-24 13:28:58 -0400

    feat: class midia

---
+++ 1_Sistema_de_Midias_Educacionais/midia.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+class Midia(ABC):
+    def __init__(self, titulo, duracao):
+        self.titulo = titulo
+        self.duracao = duracao
+
+    def mostrar_info(self):
+        print(f"Título: {self.titulo}")
+        print(f"Duração: {self.duracao}")
+    
+    @abstractmethod
+    def reproduzir(self):
+        pass
+
+
+class Video(Midia):
+    def __init__(self, titulo, duracao, resolucao):
+        super.__init__(titulo, duracao)
+        self.resolucao = resolucao
+
+    def reproduzir(self):
+        print(f"Reproduzindo vídeo: {self.titulo} com resolução {self.resolucao}")
+
+class Podcast(Midia):
+    def __init__(self, titulo, duracao, apresentador):
+        super().__init__(titulo, duracao)
+        self.apresentador = apresentador
+
+    def reproduzir(self):
+        print(f"Reproduzindo podcast: {self.titulo} com apresentador {self.apresentador}")
+
+
+class TextoNarrado(Midia):
+    def __init__(self, titulo, duracao, idioma):
+        super().__init__(titulo, duracao)
+        self.idioma = idioma
+
+    def reproduzir(self):
+        print(f"Reproduzindo texto narrado: {self.titulo} com idioma {self.idioma}")
+
